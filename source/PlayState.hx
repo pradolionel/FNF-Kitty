62c2b7332b1b750513b742fa1ebb9be95e9cdde7
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-04-29 16:54:09 -0500

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -368,31 +368,13 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
-			case 'stage': //Week 1
-				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
-				add(bg);
+			case 'hill': //Week 1
+		    	var hill_bg:BGSprite = new BGSprite("hill/1382_sin_titulo_20220316203836", 0, 0, 0.9, 0.9);
+				add(hill_bg);
 
-				var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
-				stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-				stageFront.updateHitbox();
-				add(stageFront);
-
-				if(!ClientPrefs.lowQuality) {
-					var stageLight:BGSprite = new BGSprite('stage_light', -125, -100, 0.9, 0.9);
-					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-					stageLight.updateHitbox();
-					add(stageLight);
-					var stageLight:BGSprite = new BGSprite('stage_light', 1225, -100, 0.9, 0.9);
-					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-					stageLight.updateHitbox();
-					stageLight.flipX = true;
-					add(stageLight);
-
-					var stageCurtains:BGSprite = new BGSprite('stagecurtains', -500, -300, 1.3, 1.3);
-					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
-					stageCurtains.updateHitbox();
-					add(stageCurtains);
-				}
+				sun = new BGSprite("hill/sun", 1643, -408, 1.1, 1.1, ["sun"]);
+				sun.animation.stop();
+				add(sun);
 
 			case 'spooky': //Week 2
 				if(!ClientPrefs.lowQuality) {
@@ -3862,6 +3844,9 @@ class PlayState extends MusicBeatState
 				if(!ClientPrefs.lowQuality) {
 					bgGirls.dance();
 				}
+				
+			case 'hill':
+				sun.animation.play("sun", true);
 
 			case 'mall':
 				if(!ClientPrefs.lowQuality) {

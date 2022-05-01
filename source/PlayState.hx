b5f0dbe7f85dfda631bffa58c23a06a9c8d9cc50
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-04-30 19:45:55 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -217,6 +217,8 @@ class PlayState extends MusicBeatState
 	var bgGirls:BackgroundGirls;
 	var wiggleShit:WiggleEffect = new WiggleEffect();
 	var bgGhouls:BGSprite;
+	
+	var kid:BGSprite;
 
 	public var songScore:Int = 0;
 	public var songHits:Int = 0;

32c8ff50b6b879f67140c4bde52b0da322483298
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-02 19:13:33 -0500

    Add files via upload

--- source/StageData.hx
+++ source/StageData.hx
@@ -1,11 +1,6 @@
 package;
 
-#if MODS_ALLOWED
-import sys.io.File;
-import sys.FileSystem;
-#else
 import openfl.utils.Assets;
-#end
 import haxe.Json;
 import haxe.format.JsonParser;
 import Song;
@@ -20,15 +15,6 @@ typedef StageFile = {
 	var boyfriend:Array<Dynamic>;
 	var girlfriend:Array<Dynamic>;
 	var opponent:Array<Dynamic>;
-	var hide_girlfriend:Bool;
-	
-	var static_camera:Bool;
-	var static_camera_pos:Array<Float>;
-
-	var camera_boyfriend:Array<Float>;
-	var camera_opponent:Array<Float>;
-	var camera_girlfriend:Array<Float>;
-	var camera_speed:Null<Float>;
 }
 
 class StageData {
@@ -73,18 +59,9 @@ class StageData {
 		var rawJson:String = null;
 		var path:String = Paths.getPreloadPath('stages/' + stage + '.json');
 
-		#if MODS_ALLOWED
-		var modPath:String = Paths.modFolders('stages/' + stage + '.json');
-		if(FileSystem.exists(modPath)) {
-			rawJson = File.getContent(modPath);
-		} else if(FileSystem.exists(path)) {
-			rawJson = File.getContent(path);
-		}
-		#else
 		if(Assets.exists(path)) {
 			rawJson = Assets.getText(path);
 		}
-		#end
 		else
 		{
 			return null;

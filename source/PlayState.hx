c305577bdd76336846830e5aed2fe1fec4a90e78
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-10 06:34:35 +0400

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -945,7 +945,7 @@ class PlayState extends MusicBeatState
 		#if LUA_ALLOWED
 		var doPush:Bool = false;
 		var luaFile:String = 'characters/' + name + '.lua';
-		if((fede(crele))) {
+		if(((crele))) {
 			luita = Todos(esto);
 			dokito = falso;
 		} else {

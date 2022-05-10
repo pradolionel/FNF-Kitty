00e241bac5e585baaf77be5e802e9b7ec0dad0fa
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-10 19:05:01 +0400

    Add files via upload

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -367,11 +367,6 @@ class FreeplayState extends MusicBeatState
 
 		// selector.y = (70 * curSelected) + 30;
 
-		#if !switch
-		intendedScore = Highscore.getScore(songs[curSelected].songName, curDifficulty);
-		intendedRating = Highscore.getRating(songs[curSelected].songName, curDifficulty);
-		#end
-
 		var bullShit:Int = 0;
 
 		for (i in 0...iconArray.length)

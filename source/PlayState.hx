23ff2e1ee8608da8f83fc4a76dc5d3ff9abd8e23
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-14 12:14:08 -0500

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -563,6 +563,7 @@ class PlayState extends MusicBeatState
 		startCharacterLua(boyfriend.curCharacter);	
 
 		var camPos:FlxPoint = new FlxPoint(girlfriendCameraOffset[0], girlfriendCameraOffset[1]);
+		if(gf != null)
 		{
 			camPos.x += gf.getGraphicMidpoint().x + gf.cameraPosition[0];
 			camPos.y += gf.getGraphicMidpoint().y + gf.cameraPosition[1];
@@ -570,7 +571,8 @@ class PlayState extends MusicBeatState
 
 		if(dad.curCharacter.startsWith('gf')) {
 			dad.setPosition(GF_X, GF_Y);
-			gf.visible = false;
+			if(gf != null)
+				gf.visible = false;
 		}
 
 		switch(curStage)

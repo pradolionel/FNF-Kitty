a2c3f7d31d407e03eb2fd131754e12a146ba0deb
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-14 22:26:16 -0500

    Add files via upload

---
+++ assets/preload/data/stars-of-darkness/script.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', "PORT BY SOPUNIC508", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 50);
+    addLuaText('watermark');
+end

5f5bd2e5a04a7b996b843fc6c6aa031d88d0ce4f
LionelMessiKB100 <99144412+pradolionel@users.noreply.github.com>
2024-08-28 18:44:54 -0500

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.graphics.frames.FlxAtlasFrames;
 import openfl.utils.AssetType;
+import lime.utils.AssetType;
 import openfl.Assets;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
